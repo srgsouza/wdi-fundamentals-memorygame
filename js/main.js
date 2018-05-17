1c82622fcea45a097da337947e8e341368458cc4
Sergio Souza <emailsouza@gmail.com>
2018-05-16 21:18:39 -0600

    created game logig

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-console.log("Up and running!");
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+let cards = ['queen', 'queen', 'king', 'king'];
+let cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[2];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
 
+cardsInPlay.push(cardOne);
+console.log(`User flipped ${cardOne}`);
+
+cardsInPlay.push(cardTwo);
+console.log(`User flipped ${cardTwo}`);
+
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert('You found a match');
+    } else {
+        alert('Sorry. Try again');
+    }
+}
 
