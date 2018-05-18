3cafd2d2ee43b2784e6aaf81d7648cd466fd0131
Sergio Souza <emailsouza@gmail.com>
2018-05-18 11:18:31 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
-
 let cards = ['queen', 'queen', 'king', 'king'];
 let cardsInPlay = [];
-let cardOne = cards[0];
-let cardTwo = cards[2];
 
-
-cardsInPlay.push(cardOne);
-console.log(`User flipped ${cardOne}`);
-
-cardsInPlay.push(cardTwo);
-console.log(`User flipped ${cardTwo}`);
-
-if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert('You found a match');
-    } else {
-        alert('Sorry. Try again');
+var checkForMatch = function () {
+    if (cardsInPlay.length === 2) {
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+            alert('You found a match');
+        } else {
+            alert('Sorry. Try again');
+        }
     }
 }
+
+var flipCard = function (cardId) {
+    cardsInPlay.push(cards[cardId]);
+    console.log(`User flipped ${cards[cardId]}`);
+    checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
 
